958e490c2d49c7e4f42bc7f748a8a8cc65c629ed
Andrew <andrewchoi1@hotmail.com>
2018-07-31 12:00:03 -0400

    refactored code

--- index.js
+++ index.js
@@ -1,43 +1,77 @@
 var form = document.querySelector('[name="coffeeOrderForm"]');
+var containersArray = [];
+var ordersList = document.querySelector('.orders-list');
+var updateLocalStorage = function() {
+    localStorage.setItem('orders', JSON.stringify(containersArray));
+};
+
+var updateOrders = function() {
+    containersArray.forEach(function (order) {
+        var unorderedList = document.createElement('ul');
+        unorderedList.classList.add('unorderedList')
+        var coffeeContainer = document.createElement('li');
+        var emailContainer = document.createElement('li');
+        var sizeContainer = document.createElement('li');
+        var flavorShotContainer = document.createElement('li');
+        var caffeineRatingContainer = document.createElement('li');
+
+        coffeeContainer.textContent = order.coffee;
+        emailContainer.textContent = order.email;
+        sizeContainer.textContent = order.size;
+        flavorShotContainer.textContent = order.flavorShot;
+        caffeineRatingContainer.textContent = order.caffeineRating;
+
+        unorderedList.appendChild(coffeeContainer);
+        unorderedList.appendChild(emailContainer);
+        unorderedList.appendChild(sizeContainer);
+        unorderedList.appendChild(flavorShotContainer);
+        unorderedList.appendChild(caffeineRatingContainer);
+
+        ordersList.appendChild(unorderedList);
+    });
+};
+
+var getfromLocalStorage = function() {
+    var getOrders = localStorage.getItem('orders');
+    var ordersObj = JSON.parse(getOrders);
+    if (ordersObj !== null) {
+        ordersObj.forEach(function(order) {
+            containersArray.push(order)
+        });
+    };
+};
+
+
+var clearOrders = function() {
+    var pendingOrders = document.querySelectorAll('.unorderedList');
+    pendingOrders.forEach(function(order) {
+        order.remove()
+    });
+};
+
 
 form.addEventListener('submit', function(e) {
+    e.preventDefault();
     var coffeeOrder = document.querySelector('[name="coffeeOrder"]');
     var email = document.querySelector('[name="email"]');
     var size = document.querySelector('[name="size"]');
     var flavorShot = document.querySelector('[name="flavorShot"]');
     var caffeineRating = document.querySelector('[name="caffeineRating"]');
 
-    e.preventDefault();
+    var orders = {
+        coffee: coffeeOrder.value,
+        email: email.value,
+        size: size.value,
+        flavorShot: flavorShot.value,
+        caffeineRating: caffeineRating.value
+    };
 
-    var coffeeContainer = document.createElement('li');
-    var emailContainer = document.createElement('li');
-    var sizeContainer = document.createElement('li');
-    var flavorShotContainer = document.createElement('li');
-    var caffeineRatingContainer = document.createElement('li');
-
-    var unorderedList = document.createElement('ul');
-    var order = document.createElement('li');
-
-    unorderedList.classList.add('unorderedList');
-
-    coffeeContainer.textContent = coffeeOrder.value;
-    emailContainer.textContent = email.value;
-    sizeContainer.textContent = size.value;
-    flavorShotContainer.textContent = flavorShot.value;
-    caffeineRatingContainer.textContent = caffeineRating.value;
-
-    unorderedList.appendChild(coffeeContainer);
-    unorderedList.appendChild(emailContainer);
-    unorderedList.appendChild(sizeContainer);
-    unorderedList.appendChild(flavorShotContainer);
-    unorderedList.appendChild(caffeineRatingContainer);
-
-    order.appendChild(unorderedList);
-
-    document.querySelector('.orders-list').appendChild(order);
-
-    // var ordersList = document.querySelectorAll('.unorderedList');
-    // var ordersJSON = JSON.stringify(ordersList);
-    // localStorage.setItem('order', ordersJSON);
-    // console.log(localStorage)
+    containersArray.push(orders);
+    updateLocalStorage();
+    clearOrders();
+    updateOrders();
+    console.log(localStorage);
 });
+
+getfromLocalStorage();
+updateOrders();
